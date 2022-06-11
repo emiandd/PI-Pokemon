26ce4d3149fb47d4b42cd556255e3da3ad01a310
emiandd <emilandresaravena@icloud.com>
2022-06-11 18:33:11 -0400

    pagination frontend working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import Landing from './components/Landing/Landing.jsx';
+import Home from './components/Home/Home.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <Route exact path="/" component={Landing} />
+      <Route path='/home' component={Home} />
     </div>
   );
 }
