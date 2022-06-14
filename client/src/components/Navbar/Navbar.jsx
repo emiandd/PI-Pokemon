8fc92e7311742824b55ee36472342150faa5233e
emiandd <emilandresaravena@icloud.com>
2022-06-14 10:32:24 -0400

    filter by db or api working, filter by type working

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,34 +1,68 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Navbar.css';
+import SearchBar from '../SearchBar/SearchBar.jsx';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllPokemons, getTypes, filteredByType, filteredByDBorAPI } from '../../redux/actions.js';
 
-export default function Navbar() {
+export default function Navbar({setCurrentPage}) {
+
+	const dispatch = useDispatch();
+	const pokemonTypes = useSelector( state => state.allTypes);
+
+	// console.log(pokemonTypes.map( t => t.name ));
+
+	useEffect( () =>  {
+		dispatch(getTypes())
+	}, [dispatch])
+
+
+	function clickHome(){
+		dispatch(getAllPokemons());
+	}
+
+	function handleChangeSelectByType(e){
+		// console.log(e.target.value);
+		dispatch(filteredByType(e.target.value));
+		setCurrentPage(1);
+	}
+
+	function handleFilteredByDBorAPI(e){
+		console.log(e.target.value)
+		dispatch(filteredByDBorAPI(e.target.value));
+		setCurrentPage(1);
+	}
+
+	let actualURL = window.location.href;
+
 	return (
 		<nav className='navbar'>
 			<div className='top-nav'>
 				<div className='logotipo'>
-					<h1>Poke App</h1>
+				    <Link to='/home' onClick={(e) => clickHome(e)}>
+						<h1>Poke App</h1>
+					</Link>
 				</div>	
-				<div>
-					<input type="text" placeholder="Type a Pokemon Name" />
-					<button>Search</button>
+				<div className={actualURL.includes('detail') ? 'displayNone' : 'search'}>
+					<SearchBar />
 				</div>
 				<div>
 					<button>Create a Pokemon!</button>
 				</div>
 			</div>
 			
-			<div className='bottom-nav'>
+			<div className={actualURL.includes('detail') ? 'displayNone' : 'bottom-nav'}>
 				<div>
-					<select name="types" id="1">
-						<option value="types">Types</option>
-						<option value="ground">Ground</option>
-						<option value="electric">Electric</option>
-						<option value="water">Water</option>
+					<select onChange={(e) => handleChangeSelectByType(e)} name="types" id="1">
+						<option value="All">-- All --</option>
+						{pokemonTypes?.map( t => 
+							<option value={t.name}>{t.name}</option>
+						)}
 					</select>
-					<select name="pokemons" id="1">
-						<option value="types">All Pokemons</option>
-						<option value="types">Originals</option>
-						<option value="ground">Created By Me</option>
+					<select onChange={ (e) => handleFilteredByDBorAPI(e) }  name="pokemons" id="1">
+						<option value="All">-- All --</option>
+						<option value="originals">Originals</option>
+						<option value="created">Created By Me</option>
 					</select>
 				</div>
 				<div className='ordering'>
