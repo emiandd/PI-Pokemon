0102cc3aa59098f95c6adad05ab265636d5d4e82
emiandd <emilandresaravena@icloud.com>
2022-06-18 14:40:45 -0400

    classes updated to css module

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import styles from './Card.css'
+import s from './Card.module.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { resetCards } from '../../redux/actions.js';
 
 export default function Card(props) {
+	
 	return (
-		<div className='card'>
-			<div className='img-container'>
+		<div className={s.card}>
+			<div className={s.imgContainer}>
 				<img src={props.image} alt="pokemon image" />
 			</div>
-			<h3 className='name'>{props.name}</h3>
-			<div className='types-container'>
-			{props.types?.map(t => (
-				<div className='type-bg'>
-					<p className='type-text'>{t}</p>
+			<h3 className={s.name}>{props.name}</h3>
+			<div className={s.typesContainer}>
+			{props.types?.slice(0,2).map(t => (
+				<div className={s.typeBg} id={t.name}>
+					<p className={s.typeText}>{t.name}</p>
 				</div>
 			))}
 			</div>
