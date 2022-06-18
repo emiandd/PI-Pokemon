f8fa74d8b6861c9ee41605a5025b9eae7de8ddb6
emiandd <emilandresaravena@icloud.com>
2022-06-18 15:20:45 -0400

    bug in filter created by me fixed up

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -6,24 +6,17 @@ import Loader from '../Loader/Loader.jsx';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllPokemons } from '../../redux/actions.js'
 
-
-
 export default function Home() {
 
 	const dispatch = useDispatch();
 	const allPokemons = useSelector( state => state.allPokemons);
 	const pokemonByName = useSelector( state => state.pokemonByName);
 
-	// console.log(pokemonByName);
-
-
 	const [currentPage, setCurrentPage] = useState(1);
 	const [pokemonsPerPage, setPokemonsPerPage] = useState(12);
 	const indexLastPokemon = currentPage * pokemonsPerPage;
 	const indexFirstPokemon = indexLastPokemon - pokemonsPerPage;
-	const currentPokemons =  allPokemons.length > 1 ? allPokemons.slice(indexFirstPokemon, indexLastPokemon) : [allPokemons];
-
-	// console.log(currentPokemons);
+	const currentPokemons =  allPokemons.length >= 1 ? allPokemons.slice(indexFirstPokemon, indexLastPokemon) : [allPokemons];
 
 	const paginated = (pageNumber) => {
 		setCurrentPage(pageNumber);
