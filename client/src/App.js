238974f618bb3958f7a155889a6361c4a51a60ed
emiandd <emilandresaravena@icloud.com>
2022-06-18 14:06:52 -0400

    routes: detail, createpokemon created

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing.jsx';
 import Home from './components/Home/Home.jsx';
+import PokemonDetail from './components/PokemonDetail/PokemonDetail.jsx';
+import Form from './components/Form/Form.jsx';
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Landing} />
       <Route path='/home' component={Home} />
+      <Route path='/detail/:id' component={PokemonDetail} />
+      <Route path='/createpokemon' component={Form} />
     </div>
   );
 }
