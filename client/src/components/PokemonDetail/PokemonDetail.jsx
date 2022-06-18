24a621f228f6fb05fd179d4a158ba4b6d0f14589
emiandd <emilandresaravena@icloud.com>
2022-06-18 15:35:05 -0400

    loader added before loading details

--- client/src/components/PokemonDetail/PokemonDetail.jsx
+++ client/src/components/PokemonDetail/PokemonDetail.jsx
@@ -1,33 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../Navbar/Navbar.jsx';
+import Loader from '../Loader/Loader.jsx'
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { getDetailPokemon } from '../../redux/actions.js';
-import './PokemonDetail.css';
+import { getDetailPokemon, resetDetail } from '../../redux/actions.js';
+import s from './PokemonDetail.module.css';
 
 export default function PokemonDetail() {
 
 	const { id } = useParams();
 	const dispatch = useDispatch();
-
-	useEffect(() => {
-		dispatch(getDetailPokemon(id));
-	},[dispatch])
-
+	const loader = useSelector( state => state.loader );
 	const pokemonDetail = useSelector( state => state.pokemonDetail );
 
+	useEffect(() => {
+		if(!loader){
+			dispatch(getDetailPokemon(id));
+		}
+		return () => {
+			dispatch(resetDetail());
+		}
+
+	},[dispatch])
 
 	return (
 		<div>
 			<Navbar />
-			<div className='detail-container'>
-				<div className='detail-left'>
-					<img src={pokemonDetail.image} alt="pokemon image" />
+
+			{loader ? <Loader /> 
+			: <div className={s.detailContainer}>
+				<div className={s.detailLeft}>
+					<img src={pokemonDetail.image} alt={`${pokemonDetail.name} image`} />
 				</div>
-				<div className='detail-right'>
+				<div className={s.detailRight}>
 					<h1>{pokemonDetail.name}</h1>
 					<p>ID: {pokemonDetail.id}</p>
-					<div className='info'>
+					<div className={s.info}>
 					    <div>
 							<h3>Life</h3>
 							<p>{pokemonDetail.life}</p>
@@ -50,13 +58,13 @@ export default function PokemonDetail() {
 							<p>{pokemonDetail.weight}</p>
 						</div>
 					</div>
-					<div className='types'>
+					<div className={s.types}>
 					{pokemonDetail.types?.map(t => (
-						<p className='type-text-detail'>{t}</p>
+						<p className={s.typeTextDetail} id={t.name}>{t.name}</p>
 					))}
 					</div>
 				</div>
-			</div>
+			</div>}
 		</div>
 	)
 }
